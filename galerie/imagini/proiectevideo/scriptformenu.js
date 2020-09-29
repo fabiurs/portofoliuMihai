9bbb3d956aa07bc2228ba76b49352d58306fb754
Fabian Ursache <fabi24ursache@gmail.com>
2020-09-29 17:38:37 +0300

    updated menu

--- galerie/imagini/proiectevideo/scriptformenu.js
+++ galerie/imagini/proiectevideo/scriptformenu.js
@@ -56,3 +56,43 @@ function toggleMenu(){
 
     }
 }
+
+
+var c = document.querySelector(".cover");
+var i1 = document.getElementById("img1");
+var i2 = document.getElementById("img2");
+var i3 = document.getElementById("img3");
+var i4 = document.getElementById("img4");
+function imagineBackground(nr){
+    setTimeout(() => {
+        if( nr == 1 ){
+            i1.style.opacity = "1";
+        }
+        else if( nr == 2 ){
+            i2.style.opacity = "1";
+        }
+        else if( nr == 3 ){
+            i3.style.opacity = "1";    
+        }
+        else if( nr == 4 ){
+            i4.style.opacity = "1";
+        }
+    }, 10);
+}
+
+function stergeImagine(nr){
+    setTimeout(() => {
+        if( nr == 1 ){
+            i1.style.opacity = "0";
+        }
+        else if( nr == 2 ){
+            i2.style.opacity = "0";
+        }
+        else if( nr == 3 ){
+            i3.style.opacity = "0";    
+        }
+        else if( nr == 4 ){
+            i4.style.opacity = "0";
+        }
+    }, 10);
+}
